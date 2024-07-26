69ed4b4b1f7482d3cd42e7f6ffb58b1d05841a8a
AnujAgrawal3107 <107804980+AnujAgrawal3107@users.noreply.github.com>
2024-07-27 02:57:15 +0530

    cors updated

--- server.js
+++ server.js
@@ -46,12 +46,13 @@ app.set('view engine', 'ejs');
 //   next();
 // });
 app.use((req, res, next) => {
-  res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
-  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, DELETE');
+  res.setHeader('Access-Control-Allow-Origin', 'https://virtual-classroom-server-yump.onrender.com');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, DELETE, OPTIONS');
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
   res.setHeader('Access-Control-Allow-Credentials', 'true');
   next();
 });
+
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
